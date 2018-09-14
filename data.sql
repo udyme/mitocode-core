eed2774d698944a04315e029e8cb9c2bff9337b6
Alex Pariona <LxPary@users.noreply.github.com>
2018-09-14 18:10:24 -0500

    -

---
+++ data.sql
@@ -0,0 +1,163 @@
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (1, 'Buscar', 'search', '/buscar');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (2, 'Registrar', 'insert_drive_file', '/consulta');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (3, 'Registrar E.', 'insert_drive_file', '/consulta-especial');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (4, 'Especialiades', 'star_rate', '/especialidad');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (5, 'Médicos', 'healing', '/medico');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (6, 'Examenes', 'assignment', '/examen');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (7, 'Pacientes', 'accessibility', '/paciente');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (8, 'Reportes', 'assessment', '/reporte');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (9, 'Signos Vitales', 'assessment', '/signos');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (10, 'Menu', 'assessment', '/menu');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (11, 'Roles', 'assessment', '/roles');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (12, 'Asignar Roles a Menu', 'assessment', '/menu-rol');
+INSERT INTO menu(id_menu, nombre, icono, url) VALUES (13, 'Asignar Rol Usuario', 'assessment', '/usuario-rol');
+
+select * from paciente;
+
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CARLOS', 'RUIZ', '72302301' , '991855511');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('ANDRES', 'RUIZ', '72301302' , '991855512');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('ANA', 'RUIZ', '72301303' , '991855513');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MARIA', 'RUIZ', '72301304' , '991855514');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('KAREN', 'RUIZ', '72301305' , '991855512');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('LUIS', 'RUIZ', '72301306' , '991855513');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('JOSE', 'RUIZ', '72301307' , '991855514');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('EDGAR', 'RUIZ', '72301308' , '991855588');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('ROBERTO', 'RUIZ', '72301309' , '991855522');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MIA', 'RUIZ', '72301310' , '991855566');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('JEREMY', 'RUIZ', '72301322' , '991855557');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('PEPE', 'RUIZ', '72301312' , '991855541');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('KATHY', 'RUIZ', '72301313' , '991855565');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MANUEL', 'RUIZ', '72301314' , '991855561');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MARCOS', 'RUIZ', '72301316' , '991855562');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('KARLA', 'RUIZ', '72301391' , '991855563');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CARLOS', 'RUIZ', '72301801' , '991855564');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('EDUARDO', 'RUIZ', '72308301' , '991855565');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('LESTER', 'RUIZ', '72301901' , '991855566');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('ERIKA', 'RUIZ', '72301381' , '991855567');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('TOMAS', 'RUIZ', '72301311' , '991855568');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('TOMASA', 'RUIZ', '72301491' , '991855569');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('TITO', 'RUIZ', '72301353' , '991855570');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('YAHAIRA', 'RUIZ', '72301341' , '991855571');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('BRAYAN', 'RUIZ', '72301342' , '991855572');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('KEVIN', 'RUIZ', '72301343' , '991855573');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('BRITTANY', 'RUIZ', '72301344' , '991855574');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('JULIO', 'RUIZ', '72301345' , '991855575');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('JULIA', 'RUIZ', '72301346' , '991855576');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CESAR', 'RUIZ', '72301347' , '991855577');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('PAOLO', 'RUIZ', '72301348' , '991855578');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('PAOLA', 'RUIZ', '72301349' , '991855579');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('YOSHIMAR', 'RUIZ', '72301350' , '991855580');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('LIONEL', 'RUIZ', '72301351' , '991855581');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('SERGIO', 'RUIZ', '72301352' , '991855582');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('JOHAN', 'RUIZ', '72301353' , '991855583');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('NEYMAR', 'RUIZ', '72301354' , '991855584');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('LUKA', 'RUIZ', '72301355' , '991855585');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('GERARD', 'RUIZ', '72301356' , '991855586');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('RAFAEL', 'RUIZ', '72301357' , '991855587');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('LUIS', 'RUIZ', '72301358' , '991855588');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MIGUEL', 'RUIZ', '72301359' , '991855589');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CRISTIAN', 'RUIZ', '72301360' , '991855590');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CRISTINA', 'RUIZ', '72301361' , '991855591');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('PEPA', 'RUIZ', '72301362' , '991855592');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('PEPI', 'RUIZ', '72301363' , '991855593');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('PEPO', 'RUIZ', '72301364' , '991855594');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('PEPU', 'RUIZ', '72301365' , '991855595');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MARA', 'RUIZ', '72301366' , '991855596');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CLAUDIO', 'RUIZ', '72309367' , '991855597');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CLAUDIA', 'RUIZ', '72301368' , '991855598');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('EDSON', 'RUIZ', '72301369' , '991855599');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('HUGO', 'RUIZ', '72301370' , '991855544');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('HUMBERTO', 'RUIZ', '72301371' , '991855541');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('FIORELA', 'RUIZ', '72301372' , '991855542');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('TIFFANY', 'RUIZ', '72301373' , '991855543');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('CHUKY', 'RUIZ', '72301374' , '991855545');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('FEFE', 'RUIZ', '72301375' , '991855546');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MARQUINHOS', 'RUIZ', '72301376' , '991855547');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MAX', 'RUIZ', '72301377' , '991855548');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MAXA', 'RUIZ', '72301378' , '991855549');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MAXI', 'RUIZ', '72301379' , '991855550');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('MAXO', 'RUIZ', '72301380' , '991855571');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('GUSTAVO', 'RUIZ', '72301381' , '991855572');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('DELIA', 'RUIZ', '72301382' , '991855573');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('DEWI', 'RUIZ', '72301383' , '991855574');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('KUN', 'RUIZ', '72301384' , '991855575');
+INSERT INTO paciente(nombres, apellidos, dni, telefono) VALUES('AGUERO', 'RUIZ', '72302384' , '991855575');
+
+select * from usuario;
+
+INSERT INTO usuario(id_usuario, nombre, clave, estado) values (1, 'admin', '123', '1');
+INSERT INTO usuario(id_usuario, nombre, clave, estado) values (2, 'jaime', '123', '1');
+
+select * from Rol;
+INSERT INTO Rol (id_rol, nombre, descripcion) VALUES (1, 'ADMIN', 'Administrador');
+INSERT INTO Rol (id_rol, nombre, descripcion) VALUES (2, 'USER', 'Usuario');
+INSERT INTO Rol (id_rol, nombre, descripcion) VALUES (3, 'DBA', 'Adminsitrador de base de datos');
+
+INSERT INTO usuario_rol (id_usuario, id_rol) VALUES (1, 1);
+INSERT INTO usuario_rol (id_usuario, id_rol) VALUES (1, 3);
+INSERT INTO usuario_rol (id_usuario, id_rol) VALUES (2, 2);
+
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (1, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (2, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (3, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (4, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (5, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (6, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (7, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (8, 1);
+
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (9, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (10, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (11, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (12, 1);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (13, 1);
+
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (4, 2);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (5, 2);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (6, 2);
+INSERT INTO menu_rol (id_menu, id_rol) VALUES (7, 2);
+
+
+select m.* from menu_rol mr inner join usuario_rol ur 	on ur.id_rol = mr.id_rol
+							inner join menu m 			on m.id_menu = mr.id_menu
+							inner join usuario u 		on u.id_usuario = ur.id_usuario
+where u.nombre = 'admin';
+
+
+create table oauth_access_token (
+  token_id VARCHAR(256),
+  token bytea,
+  authentication_id VARCHAR(256),
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication bytea,
+  refresh_token VARCHAR(256)
+);
+
+
+
+CREATE OR REPLACE FUNCTION fn_listarResumen ()  --param_x INT
+ RETURNS TABLE (
+ cantidad INT,
+ fecha TEXT
+)
+AS $$
+DECLARE
+    var_r record;
+BEGIN
+FOR var_r IN(
+	select (count(*)::int) as cantidad, to_char(c.fecha, 'dd/MM/yyyy') as fecha from consulta c
+	--where id_medico = param_x
+	group by c.fecha order by c.fecha asc
+	)
+ LOOP
+        cantidad := var_r.cantidad;
+ 		fecha := var_r.fecha;
+        RETURN NEXT;
+ END LOOP;
+END; $$
+LANGUAGE 'plpgsql';
+
+
+
+
